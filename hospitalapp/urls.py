bdfb72b4bb28e8dbf1a7c45a7f6316d116ea8a48
srstig <rayogutu36@gmail.com>
2025-02-27 14:05:53 +0300

    Thursday 27

--- hospitalapp/urls.py
+++ hospitalapp/urls.py
@@ -4,7 +4,11 @@ from django.urls import path
 from hospitalapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('',views.index, name = 'index'),
     path('starter/',views.starter, name = 'starter'),
+    path('about/',views.about, name = 'about'),
+    path('services/',views.services, name = 'services'),
+    path('departments/',views.departments, name = 'departments'),
+    path('doctors/',views.doctors, name = 'doctors'),
 ]
